83ab9b26b701c09187388c71a34b0b1fe86d6696
Bethsabet <bethsa1996@gmail.com>
2026-04-16 15:48:59 +0100

    Estructura inicial de protocolo-gus con Spring Boot

---
+++ src/main/java/com/protocologus/ProtocologusApplication.java
@@ -0,0 +1,13 @@
+package com.protocologus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtocologusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtocologusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/protocologus/ProtocologusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protocologus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtocologusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
